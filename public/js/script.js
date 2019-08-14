c510917451d88ace3fdec9f658c4cb731027bf90
John Bretherton <270022735@bun2-206-10.local>
2019-08-14 12:02:25 +1200

    Getting gender data from database

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,69 @@
+$(document).ready(function(){
+    $.ajax({
+    url: 'http://192.168.33.10:3000/people',
+    dataType: 'json',
+    type: 'get',
+    success:function(dataFromServer){
+      console.log(dataFromServer);
+    },
+    error:function(error){
+      console.log(error);
+      console.log('something went wrong');
+    }
+  })
+});
 
+//The Long way
+  // $('.genderRadio').click(function(){
+  //   const gender = $(this).val();
+  //   console.log(gender);
+  //   if(gender === 'Male'){
+  //     $.ajax({
+  //       url: 'http://192.168.33.10:3000/gender/g=male',
+  //       dataType: 'json',
+  //       type: 'get',
+  //       success:function(maleData){
+  //         console.log(maleData);
+  //       },
+  //       error:function(error){
+  //         console.log(error);
+  //         console.log('something went wrong');
+  //       }
+  //     })
+  //   } else {
+  //     $.ajax({
+  //       url: 'http://192.168.33.10:3000/gender/g=female',
+  //       dataType: 'json',
+  //       type: 'get',
+  //       success:function(femaleData){
+  //         console.log(femaleData);
+  //       },
+  //       error:function(error){
+  //         console.log(error);
+  //         console.log('something went wrong');
+  //       }
+  //     })
+  //   }
+  //
+  // })
+// The shorter way:
+  $('.genderRadio').click(function(){
+    const gender = $(this).val().toLowerCase();
+    $.ajax({
+      url: `http://192.168.33.10:3000/gender/g=${gender}`,
+      dataType: 'json',
+      type: 'get',
+      success:function(data){
+        console.log(data);
+      },
+      error:function(error){
+        console.log(error);
+        console.log('something went wrong');
+      }
+    })
+  })
+
+// $.ajax({url: 'http://192.168.33.10:3000/people', success: function(result){
+//     console.log(result);
+//   // $('mainBox').html(result);
+// }});
--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = express();
 const port = 3000;
 const path = require('path');
 
+const data = require('./MOCK_DATA');
+
 app.use(function(req, res, next){
     console.log(`${req.method} request for ${req.url}`);
     next();
@@ -12,9 +14,37 @@ app.use(function(req, res, next){
 // This line does tons of stuff that we previously had to use tons of if statements to do:
 app.use(express.static('./public'));
 
+app.get('/aboutProject', function(req, res){
+  res.sendFile(path.join(__dirname + '/public/about.html'));
+});
+
+
+
 app.use('/bootstrap', express.static(path.join(__dirname, 'node_modules/bootstrap/dist')));
 app.use('/jquery', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
 app.use('/popper.js', express.static(path.join(__dirname, 'node_modules/popper.js/dist')));
+
+
+app.get('/people', function(req, res){
+    res.json(data);
+});
+
+app.get('/gender/g=:gender', function(req, res){
+    const genderParam = req.params.gender;
+    if ((genderParam == 'male') || (genderParam == 'female')) {
+        let filteredData = [];
+        for (var i = 0; i < data.length; i++) {
+            if(genderParam === data[i].gender.toLowerCase()){
+                filteredData.push(data[i]);
+            }
+        }
+        res.send(filteredData);
+    } else {
+        res.send('Invalid parameter!!')
+    }
+});
+// In express, a colon plus a word is a parameter
+
 
 // app.get('/', (req, res) => res.send('Hello world'));
 //Also can use app.post();
